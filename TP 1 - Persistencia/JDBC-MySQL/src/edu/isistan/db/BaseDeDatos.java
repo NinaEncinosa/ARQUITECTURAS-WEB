3f04464d00000e96e24d447c0e24070131585425
NinaEncinosa <67855315+NinaEncinosa@users.noreply.github.com>
2021-08-25 05:49:26 -0300

    Ejercicio 2) JDBC - MySQL

--- TP 1 - Persistencia/JDBC-MySQL/src/edu/isistan/db/BaseDeDatos.java
+++ TP 1 - Persistencia/JDBC-MySQL/src/edu/isistan/db/BaseDeDatos.java
@@ -6,54 +6,33 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-//para la op1:
-//import org.apache.derby.jdbc.EmbeddedDriver;
 
 public class BaseDeDatos {
 
 	public static void main(String[] args) {
-		//para registrar el driver:
-		
-		//opc1:
-		//creo una instancia del driver y no la utilizo
-		//con esta opcion no puedo cambiar el driver sin tener que cambiar el codigo 
-		
-		//new EmbeddedDriver();
-		
-		//op2, mejor: almaceno el driver en una variable (aca hardcodeada pero la idea es que venga desde afuera)
+
 		String driver = "com.mysql.cj.jdbc.Driver";
 		
 		try {
-			//esta es la linea que escribo, despues con el cartelito de error hago que se genere el try/catch automatico
 			Class.forName(driver).getDeclaredConstructor().newInstance();
 		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
 				| NoSuchMethodException | SecurityException | ClassNotFoundException e) {
 			e.printStackTrace();
-			//y agrego esta linea para que si surge un problema salga del programa
 			System.exit(1);
 		}
 		
-		//una vez configurado el driver, tenemos que conectarnos a la BD
-		//para esto necesitamos definir la direccion donde esta la BD:
-		//particularidades, siempre empieza con 
-		//"jdbc : tipo de la base de datos : path de la BD o poner un nombre si es de cero ; parametro que indica que si no existe la cree "
-		
 		//path usando docker 3306
-		//CHECKEAR QUE PROBANDO CREE LA TABLA PERSONA EN DATABASE "MYSQL", borrarla!
-		//String uri = "jdbc:mysql://localhost:3306/example";
+		String uri = "jdbc:mysql://localhost:3306/example";
 		
 		//path usando MAMP
-		String uri = "jdbc:mysql://localhost:8889/example";
+		//String uri = "jdbc:mysql://localhost:8889/example";
 		
-		//una vez definida la uri, creamos la conexion 
-		//selecciono la connection que viene de java.sql
-		//como puede surgir un problema, rodeo con try catch
 		try {
 			//abro la conexion con docker
-			//Connection conn = DriverManager.getConnection(uri, "root", "password");
+			Connection conn = DriverManager.getConnection(uri, "root", "password");
 			
 			//abro la conexion con MAMP
-			Connection conn = DriverManager.getConnection(uri, "root", "root");
+			//Connection conn = DriverManager.getConnection(uri, "root", "root");
 			
 			conn.setAutoCommit(false);
 			
@@ -62,9 +41,7 @@ public class BaseDeDatos {
 			
 			addPerson(conn,1,"Nina",24);
 			addPerson(conn,2,"Carlos",53);
-			
-			
-			//cierro la conexion
+
 			conn.close();
 			
 		} catch (SQLException e) {
